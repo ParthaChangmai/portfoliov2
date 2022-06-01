100d2a9cfdaece2249a77b2f318bc985a74303be
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-06-01 16:52:30 +0530

    fixed the mobile nav width issue

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { FiMenu } from 'react-icons/fi';
 import { BsGithub } from 'react-icons/Bs';
 import { AiFillLinkedin, AiFillMail, AiOutlineClose } from 'react-icons/Ai';
+import Social from './Social';
 
 const Navbar = () => {
 	const [activeNav, setActiveNav] = useState(false);
@@ -10,7 +11,7 @@ const Navbar = () => {
 
 	return (
 		<>
-			<div className="min-w-screen flex justify-between bg-[#182647] h-16 items-center font-bold  shadow-md shadow-slate-300">
+			<div className="min-w-screen flex justify-between bg-[#182647]/10 h-16 items-center font-bold  shadow-md shadow-slate-300">
 				<div className="pl-3">icon</div>
 				<div>
 					<ul className="hidden md:flex gap-3 pr-4 ">
@@ -27,54 +28,51 @@ const Navbar = () => {
 			</div>
 
 			{/* mobile nav starts here */}
+
 			<div
 				className={
-					activeNav ? 'fixed left-0 top-0 w-full min-h-screen bg-white/60 ' : ''
+					activeNav
+						? 'fixed flex flex-col h-screen justify-between left-0 top-0 w-full bg-black  p-10 ease-in duration-500  '
+						: 'fixed flex flex-col h-screen justify-between left-[-100%] top-0  p-10 ease-in duration-500  '
 				}
 			>
-				<div
-					className={
-						activeNav
-							? 'fixed flex flex-col h-screen justify-between left-0 top-0 w-full bg-black min-h-screen p-10 ease-in duration-500  '
-							: 'fixed flex flex-col h-screen justify-between left-[-100%] top-0 min-h-screen p-10 ease-in duration-500  '
-					}
-				>
-					<div className="flex justify-end ">
-						<AiOutlineClose
-							onClick={toggleNav}
-							className="bg-gray-900 p-2 text-3xl rounded-full shadow-lg shadow-indigo-600 cursor-pointer"
-						/>
+				<div className="flex justify-end ">
+					<AiOutlineClose
+						onClick={toggleNav}
+						className="bg-gray-900 p-2 text-3xl rounded-full shadow-lg shadow-indigo-600 cursor-pointer"
+					/>
+				</div>
+				<div>
+					<ul className="gap-5 pr-4 flex flex-col items-center">
+						<li className="text-2xl font-bold p-4 py-5 ease-in duration-100">
+							Home
+						</li>
+						<li className="text-2xl font-bold p-4 py-5 ease-in duration-100">
+							About Me
+						</li>
+						<li className="text-2xl font-bold p-4 py-5 ease-in duration-100">
+							Portfolio
+						</li>
+						<li className="text-2xl font-bold p-4 py-5 ease-in duration-100">
+							Contacts
+						</li>
+					</ul>
+				</div>
+
+				<div className="flex justify-between px-6  text-3xl">
+					<div>
+						<BsGithub className="bg-gray-900 p-1 rounded-lg shadow-md shadow-indigo-600" />
 					</div>
 					<div>
-						<ul className="gap-5 pr-4 flex flex-col items-center">
-							<li className="text-2xl font-bold p-4 py-5 ease-in duration-100">
-								Home
-							</li>
-							<li className="text-2xl font-bold p-4 py-5 ease-in duration-100">
-								About Me
-							</li>
-							<li className="text-2xl font-bold p-4 py-5 ease-in duration-100">
-								Portfolio
-							</li>
-							<li className="text-2xl font-bold p-4 py-5 ease-in duration-100">
-								Contacts
-							</li>
-						</ul>
+						<AiFillLinkedin className="bg-gray-900 p-1 rounded-lg shadow-md shadow-indigo-600" />
 					</div>
-
-					<div className="flex justify-between px-6  text-3xl">
-						<div>
-							<BsGithub className="bg-gray-900 p-1 rounded-lg shadow-md shadow-indigo-600" />
-						</div>
-						<div>
-							<AiFillLinkedin className="bg-gray-900 p-1 rounded-lg shadow-md shadow-indigo-600" />
-						</div>
-						<div>
-							<AiFillMail className="bg-gray-900 p-1 rounded-lg shadow-md shadow-indigo-600" />
-						</div>
+					<div>
+						<AiFillMail className="bg-gray-900 p-1 rounded-lg shadow-md shadow-indigo-600" />
 					</div>
 				</div>
 			</div>
+
+			<Social />
 		</>
 	);
 };
