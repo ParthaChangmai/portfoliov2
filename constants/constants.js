7e39b06765d5c0d5193b19a26c0a75d921930b5e
ParthaChangmai <partha.partha.changmai@gmail.com>
2022-06-01 22:12:02 +0530

    for project data

---
+++ constants/constants.js
@@ -0,0 +1,46 @@
+export const works = [
+	{
+		title: 'Project name',
+		description:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae nostrum sed adipisci quod consectetur quibusdam suscipit voluptate sunt quia expedita? Sunt, itaque ex maxime dolore officia est voluptatum earum voluptate.',
+		image:
+			'https://media.istockphoto.com/photos/beautiful-emeraldcolored-glacial-rivers-of-iceland-taken-from-a-picture-id1202227531?s=612x612',
+		tech: ['Next', 'React', 'xx', 'xx', 'xx'],
+		link: '',
+		github: '',
+		id: 1,
+	},
+	{
+		title: 'Project name',
+		description:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae nostrum sed adipisci quod consectetur quibusdam suscipit voluptate sunt quia expedita? Sunt, itaque ex maxime dolore officia est voluptatum earum voluptate.',
+		image:
+			'https://media.istockphoto.com/photos/beautiful-emeraldcolored-glacial-rivers-of-iceland-taken-from-a-picture-id1202227531?s=612x612',
+		tech: ['Next', 'React', 'xx', 'xx', 'xx'],
+		link: '',
+		github: '',
+		id: 2,
+	},
+	{
+		title: 'Project name',
+		description:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae nostrum sed adipisci quod consectetur quibusdam suscipit voluptate sunt quia expedita? Sunt, itaque ex maxime dolore officia est voluptatum earum voluptate.',
+		image:
+			'https://media.istockphoto.com/photos/beautiful-emeraldcolored-glacial-rivers-of-iceland-taken-from-a-picture-id1202227531?s=612x612',
+		tech: ['Next', 'React', 'xx', 'xx', 'xx'],
+		link: '',
+		github: '',
+		id: 3,
+	},
+	{
+		title: 'Project name',
+		description:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae nostrum sed adipisci quod consectetur quibusdam suscipit voluptate sunt quia expedita? Sunt, itaque ex maxime dolore officia est voluptatum earum voluptate.',
+		image:
+			'https://media.istockphoto.com/photos/beautiful-emeraldcolored-glacial-rivers-of-iceland-taken-from-a-picture-id1202227531?s=612x612',
+		tech: ['Next', 'React', 'xx', 'xx', 'xx'],
+		link: '',
+		github: '',
+		id: 4,
+	},
+];
